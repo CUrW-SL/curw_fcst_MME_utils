9af00ea74ffed8ccab58bd90a54e4e0a6e188eb8
shadhini <jshadhiniaseka@gmail.com>
2019-11-02 13:43:23 +0530

    flush gfs_d0_18 source 16 fgt 2019-10-17 18:16:33 data

--- flush_data/flush_data.py
+++ flush_data/flush_data.py
@@ -79,7 +79,7 @@ if __name__=="__main__":
         pool = get_Pool(host=CURW_FCST_HOST, port=CURW_FCST_PORT, user=CURW_FCST_USERNAME, password=CURW_FCST_PASSWORD,
                         db=CURW_FCST_DATABASE)
 
-        hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="gfs_d0_18", source_id=15,
+        hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="gfs_d0_18", source_id=16,
                                           variable_id=None, unit_id=None, station_id=None,
                                           start=None, end=None)
 
@@ -99,7 +99,7 @@ if __name__=="__main__":
         ###################################################################################
         # delete a specific timeseries defined by a given hash id and fgt from data table #
         ###################################################################################
-        fgt = "2019-10-17 20:38:36"
+        fgt = "2019-10-17 18:16:33"
         count = 0
         for id in hash_ids:
             TS.delete_timeseries(id_=id, fgt=fgt)
