bec0db0431f9f23f25874f368bb9fd6ab5b9abc5
shadhini <jshadhiniaseka@gmail.com>
2019-11-20 16:27:20 +0530

    flush newly added flo2d "daily run" data

--- flush_data/flush_data.py
+++ flush_data/flush_data.py
@@ -79,7 +79,7 @@ if __name__=="__main__":
         pool = get_Pool(host=CURW_FCST_HOST, port=CURW_FCST_PORT, user=CURW_FCST_USERNAME, password=CURW_FCST_PASSWORD,
                         db=CURW_FCST_DATABASE)
 
-        hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="gfs_d0_18", source_id=21,
+        hash_ids = get_curw_fcst_hash_ids(pool=pool, sim_tag="daily_run", source_id=None,
                                           variable_id=None, unit_id=None, station_id=None,
                                           start=None, end=None)
 
